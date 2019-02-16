79ba698a26d3105e5ff14e2b1105055cf2e859c8
blmayer <bleemayer@gmail.com>
2019-02-15 22:56:46 -0200

    Merged header files into one

--- archiver.h
+++ archiver.h
@@ -1,18 +1,27 @@
-#ifndef INDEX_H
-#define INDEX_H
+#ifndef ARCHIVER_H
+#define ARCHIVER_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* External variables */
 extern char *INDEXFILE;
+extern char *ARCHIVE;
 extern FILE *idx;
+extern FILE *arch;
 
 /* item defines a file in the index */
 struct item {
 	char *fileName;
 	long *lengths;
 };
+
+/* Function signatures */
+
+int archiveFile(char *file);
+
+int writeToArchive(char *file);
 
 /* Load all file entries from index file */
 int readIndex();
