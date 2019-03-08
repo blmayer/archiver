f552c3aa9d6ef3245e34d85a3ad83d16d6d3ec0b
blmayer <bleemayer@gmail.com>
2019-03-08 00:04:46 -0300

    Added function to read the archive at a given addr
    - Fixed typo in include

--- get.c
+++ get.c
@@ -1,4 +1,22 @@
-#include "archive.h"
+#include "archiver.h"
+
+int readArchive(long addr, long len)
+{
+	arch = fopen(ARCHIVE, "rb");
+	if (!arch) {
+		puts("Error opening archive file");
+		return -1;
+	}
+
+	/* Read archive at addr for len bytes */
+	fseek(arch, addr, SEEK_SET);
+	while (len--) {
+		printf("%c", getc(arch));
+	}
+
+	fclose(arch);
+	return 0;
+}
 
 int get(char *args[])
 {
