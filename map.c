154d2a1287940e3673a8247c7a2b1cca6c0a6c41
blmayer <bleemayer@gmail.com>
2019-07-10 22:09:23 -0300

    Implemented add and get functions

--- map.c
+++ map.c
@@ -3,26 +3,19 @@
 int add(struct node *root, char *key, char *data)
 {
 	struct node *curr = root;
-	printf("curr addr: \t%p\n", curr);
-
 	while (*key) {
-		printf("processing key %c\n", *key);
-		printf("curr %p\n", curr);
-
 		if (!curr->next[*key - 65]) {
-			puts("not curr");
 			struct node *new = malloc(sizeof(struct node));
-			printf("new addr: %p\n", new);
-
 			curr->next[*key - 65] = new;
 		}
 
 		curr = curr->next[*key - 65];
-		printf("new curr: %p\n\n", curr);
 		key++;
 	}
 
-	curr->content = data;
+	curr->content = malloc(strlen(data) + 1);
+	memcpy(curr->content, data, strlen(data) + 1);
+
 	return 1;
 }
 
@@ -31,7 +24,7 @@ char *get(struct node *root, char *key)
 	struct node *curr = root;
 	while (*key) {
 		printf("curr %p\n", curr);
-		curr = (struct node *)&curr->next[*key++ - 65];
+		curr = curr->next[*key++ - 65];
 		if (!curr) {
 			return NULL;
 		}
